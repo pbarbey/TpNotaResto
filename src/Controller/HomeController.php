ac184b6acd73268f9262fab7f5b0d3a4a78b20e3
pbarbey <pierre.barbey3@gmail.com>
2022-01-07 21:03:44 +0100

    [FEATURE] Ajout pagination des restaurants

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\RestaurantRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,14 +14,22 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(RestaurantRepository $restaurantRepo): Response
-    {
+    public function index(
+        RestaurantRepository $restaurantRepo,
+        Request $request,
+        PaginatorInterface $paginator
+    ): Response {
         $this->denyAccessUnlessGranted('ROLE_USER');
 
-        $retaurants = $restaurantRepo->findAll();
+        $query = $restaurantRepo->findAll();
+        $restaurants = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            8 /*limit per page*/
+        );
 
         return $this->render('home/index.html.twig', [
-            'restaurants' => $retaurants,
+            'restaurants' => $restaurants,
         ]);
     }
 }
